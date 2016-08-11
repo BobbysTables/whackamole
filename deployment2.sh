1097f481fb3c901a1cdb49ecadedf7fc917493c4
BobbysTables <spicymix32@ov3u841.com>
2016-08-11 16:26:20 -0500

    Update and rename deployment1.sh to deployment2.sh

---
+++ deployment2.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+###################################################################
+# PRN10259 script used to ensure proper deployment of war file 
+###################################################################
+
+dat=`date +%m/%d/%y%n%H:%M:%S`
+echo ' '
+echo "script ended at..." $dat >/tmp/warDeploymentChecked1.txt
+echo ' '
